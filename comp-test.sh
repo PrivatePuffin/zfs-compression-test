48ed7a60181b03202f135b0c00ae94f4b02b1108
Daniel Andersen <daniel@danand.de>
2019-11-29 18:50:36 +0100

    added destory/unmount for broken/canceled tests

--- comp-test.sh
+++ comp-test.sh
@@ -3,36 +3,40 @@
 
 now=$(date +%s)
 
+echo "destroy testpool and unmount ramdisk of previous broken/canceled tests"
+sudo ./zfs/cmd/zpool/zpool destroy testpool 2>&1 >/dev/null
+sudo umount -l /mnt/ramdisk 2>&1 >/dev/null 
+
 cd ./zfs
-echo "Make sure ZFS is unloaded and make is cleaned"
+echo "make sure zfs is unloaded and make is cleaned"
 sudo ./scripts/zfs.sh -u
 sudo ./scripts/zfs-helpers.sh -r
 make -s distclean >> /dev/null
 
 
-echo "Rebuilding ZFS"
+echo "rebuilding zfs"
 sh autogen.sh >> /dev/null
 ./configure --enable-debug >> /dev/null
 make -s -j$(nproc) >> /dev/null
 
-echo "Loading ZFS"
+echo "loading zfs"
 sudo ./scripts/zfs-helpers.sh -i
 sudo ./scripts/zfs.sh
 
 cd ..
 
-echo "Creating ramdisk"
+echo "creating ramdisk"
 sudo mkdir /mnt/ramdisk
 sudo mount -t tmpfs -o size=2400m tmpfs /mnt/ramdisk
 
-echo "Creating virtial pool drive"
-truncate -s 1200M /mnt/ramdisk/pooldisk.img
+echo "creating virtial pool drive"
+truncate -s 1200m /mnt/ramdisk/pooldisk.img
 
-echo "Creating zfs testpool/fs1"
+echo "creating zfs testpool/fs1"
 sudo ./zfs/cmd/zpool/zpool create testpool -f -o ashift=12  /mnt/ramdisk/pooldisk.img
 sudo ./zfs/cmd/zfs/zfs create testpool/fs1
 
-echo "Downloading and extracting enwik9 testset"
+echo "downloading and extracting enwik9 testset"
 sudo wget -nc http://mattmahoney.net/dc/enwik9.zip
 sudo unzip -n enwik9.zip
 
@@ -57,11 +61,11 @@ cd /testpool/fs1/
 chkresult=`echo "$chksum" | sha256sum --check`
 sudo rm enwik9
 cd -
-echo "Hashcheck result: $chkresult" >> ./test_results_$now.txt
+echo "hashcheck result: $chkresult" >> ./test_results_$now.txt
 done
 
 echo "compression test finished"
-echo "Destroying pool and unmounting randisk"
+echo "destroying pool and unmounting randisk"
 sudo ./zfs/cmd/zpool/zpool destroy testpool
 sudo umount -l /mnt/ramdisk
 
