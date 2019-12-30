e9ba41ad77a88b8ea488e748c72fcaae9cfa4662
Allan Jude <allan@klarasystems.com>
2019-12-30 02:33:32 +0000

    FreeBSD sed requires -i to have an optarg

--- comp-test.sh
+++ comp-test.sh
@@ -288,7 +288,7 @@ then
 						echo "$rw (de)compression results for $comp" >> "./$TESTRESULTS"
 						echo "Speed:" >> "./$TESTRESULTS"
 						fio ./tests/$io-$rw.fio --minimal --output="./TMP/$comp-$io-$rw.terse" >> /dev/null
-						sed -i '1s/^/'"$comp;$io;$rw;$compressionratio;"'/' "./TMP/$comp-$io-$rw.terse"
+						sed -i '' '1s/^/'"$comp;$io;$rw;$compressionratio;"'/' "./TMP/$comp-$io-$rw.terse"
 						bandwidth=$(awk -F ';' '{print $11}' ./TMP/$comp-$io-$rw.terse)
 						echo "$(($bandwidth/1000)) MB/s" >> "./$TESTRESULTS"
 						echo "" >> "./$TESTRESULTS"
